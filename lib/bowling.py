a37e3f1608200a4cb956e4d6e90cc743855c7d6c
sarpong <abasimisarpong@rocketmail.com>
2019-05-06 16:26:54 +0100

    refactoring roll method

--- lib/bowling.py
+++ lib/bowling.py
@@ -5,16 +5,23 @@ class Bowling:
         self.current_frame = 1
 
     def roll(self, pins):
-        if len(self.frame) == 1 and self.frame[0] + pins > 10:
-            raise Exception('You can only pin 10 bolls in a roll' )
-
-        if len(self.frame) >= 2:
-            self.frame =[]
-            self.current_frame += 1
+        self.error(pins)
+        self.check_frame_length()
+        
         self.frame.append(pins)
         self.all_rolls.append(pins)
 
     def score(self):
         result = sum(self.all_rolls)
         return result
+
+    def error(self, pins):
+        if len(self.frame) == 1 and self.frame[0] + pins > 10:
+            raise Exception('You can only pin 10 bolls in a roll' )
+
+    def check_frame_length(self):
+        if len(self.frame) >= 2:
+            self.frame =[]
+            self.current_frame += 1
+
     
